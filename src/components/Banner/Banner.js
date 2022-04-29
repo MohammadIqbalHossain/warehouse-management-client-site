44095cde49264ec203c9222b8d31b21ed10b8e30
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-04-29 21:51:59 +0600

    banner page added

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import banner from '../../images/banner.webp';
 
 const Banner = () => {
     return (
-        <div>
-            <h1>Banner</h1>
+        <div className="flex flex-col md:flex-row mx-10 items-center ">
+            <div className="md:w-[600px]">
+                <h1 className="text-4xl text-left font-bold my-4">International fulfilment solutions for you'r business needs.</h1>
+                <p className="text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque iusto labore accusantium corporis numquam unde voluptas aperiam eaque laborum, placeat temporibus ipsam esse sunt minus eius blanditiis vitae quis perferendis!</p>
+            </div>
+            <div>
+                <img src={banner} alt="bannerVector" />
+            </div>
         </div>
     );
 };
